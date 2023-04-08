e3d9bae0653582e06bce278dcfb3cf0780de3039
Elvan Gaunder <egaunder@gmail.com>
2023-04-08 07:30:08 -0700

    feat: add tailwind css

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
+import './globals.css';
+
 export default function Home() {
   return (
     <main className='main-container'>
-      <h1>Lift-Logger</h1>
+      <h1 className='text-3xl font-bold underline'>Lift-Logger!</h1>
     </main>
   )
 }
